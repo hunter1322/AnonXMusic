c52bcfe20ec8cea4d71f9582518bdbfc3d86abfa
hunter1322 <89287045+hunter1322@users.noreply.github.com>
2022-11-27 15:37:09 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -25,8 +25,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/TheAnonymous2005/Ano
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/DevilsHeavenMF")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/denvil_bots")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Elisha_support")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
